77f4529dcd5feb41394e5cee0c419cb002ed0abb
pakkanep <patrikpakkanen9@gmail.com>
2023-11-13 14:42:01 +0200

    task 3

--- viikko2/project-reader/src/project.py
+++ viikko2/project-reader/src/project.py
@@ -1,15 +1,27 @@
 class Project:
-    def __init__(self, name, description, dependencies, dev_dependencies, license, authors):
-        self.name = name
-        self.description = description
-        self.dependencies = dependencies
-        self.dev_dependencies = dev_dependencies
-        self.license = license
+    def __init__(self, name, dependencies, licenses, authors):
+        self.name = name[0]
+        self.description = name[1]
+        self.dependencies = dependencies[0]
+        self.dev_dependencies = dependencies[1]
+        self.license = licenses
         self.authors = authors
 
-    def _stringify_dependencies(self, list):
+    def return_info(self):
+        infolist = [
+            self.name,
+            self.description,
+            self.dependencies,
+            self.dev_dependencies,
+            self.license,
+            self.authors
+        ]
+
+        return infolist
+
+    def _stringify_dependencies(self, lista):
         string = ""
-        for a in list:
+        for a in lista:
             string += "- "+a+"\n"
         return string
 
--- viikko2/project-reader/src/project_reader.py
+++ viikko2/project-reader/src/project_reader.py
@@ -1,7 +1,7 @@
-from urllib import request
-from project import Project
+from urllib.request import urlopen
+from contextlib import closing
 import toml
-
+from project import Project
 
 
 class ProjectReader:
@@ -9,22 +9,20 @@ class ProjectReader:
         self._url = url
 
     def get_project(self):
-        # tiedoston merkkijonomuotoinen sisältö
-        content = request.urlopen(self._url).read().decode("utf-8")
+        with closing(urlopen(self._url)) as response:
+            content = response.read().decode("utf-8")
         uusi = toml.loads(content)
+        return self.handle_info(uusi)
 
+    def handle_info(self, uusi):
         name = uusi["tool"]["poetry"]["name"]
         description = uusi["tool"]["poetry"]["description"]
         dependencies = uusi["tool"]["poetry"]["dependencies"]
-        dev_dependencies = uusi["tool"]["poetry"]["group"]["dev"]["dependencies"]
+        dev_dependencies =uusi["tool"]["poetry"]["group"]["dev"]["dependencies"]
         uusi_license = uusi["tool"]["poetry"]["license"]
         authors = uusi["tool"]["poetry"]["authors"]
 
-
-        # deserialisoi TOML-formaatissa oleva merkkijono ja muodosta Project-olio sen tietojen perusteella
-        return Project(name,
-                       description,
-                       dependencies,
-                       dev_dependencies,
-                       uusi_license,
-                       authors)
+        return Project((name, description),
+                    (dependencies, dev_dependencies),
+                    uusi_license,
+                    authors)
